c77dddf86e34abdbb34e7c71db15f6779eb1d3ac
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2025-10-15 23:05:51 +0330

    feat: add home page structre

---
+++ src/app/components/btn.tsx
@@ -0,0 +1,23 @@
+import Button from "@mui/material/Button";
+import { grey } from "@mui/material/colors";
+
+export default function Btn({
+  label,
+  handleClick,
+}: {
+  label: string;
+  handleClick: () => void;
+}) {
+  return (
+    <Button
+      variant="contained"
+      sx={{
+        fontSize: { xs: "1rem", md: "1.25rem", lg: "1.5rem" },
+        ":hover": { color: grey[900] },
+      }}
+      onClick={handleClick}
+    >
+      {label}
+    </Button>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
-import Button from "@mui/material/Button";
+"use client";
+
+import Typography from "@mui/material/Typography";
+import Btn from "./components/btn";
+
 export default function Home() {
   return (
     <>
-      <div className="text-[40px] bg-blue-500">سلام به شما</div>
-      <Button variant="contained" color="primary" className="bg-red-500">
-        سلام به شما
-      </Button>
+      <div className="w-full h-screen bg-gray-400 flex gap-8 flex-col items-center justify-center">
+        <Typography
+          variant="h1"
+          className="text-[2rem] md:text-[2.5rem] max-w-[500px] text-center"
+        >
+          سلام! به تسک شرکت سما رایانه خوش آمدید.
+        </Typography>
+        <Btn label="وارد به سیستم" handleClick={() => console.log("clicked")} />
+      </div>
     </>
   );
 }
